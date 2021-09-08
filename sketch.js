a61ffc1f6c62e75f853045040ad3837a04642bff
jym-tech <loredojuanamaria@gmail.com>
2021-09-08 14:30:40 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,60 +1,21 @@
-var canvas, bg;
-var together;
-var tom, tomImg1, tomImg2, tomImg3;
-var jerry, jerryImg1, jerryImg2, jerryImg3;
 
 function preload() {
-    bg = loadImage("images/garden.png");
-    tomImg1= loadAnimation("images/tomOne.png");
-    tomImg2=loadAnimation("images/tomTwo.png","images/tomThree.png");
-    tomImg3= loadAnimation("images/tomFour.png");
-    jerryImg1=loadAnimation("images/jerryOne.png");
-    jerryImg2= loadAnimation("images/jerryTwo.png","images/jerryThree.png");
-    jerryImg3=loadAnimation("images/jerryFour.png");
-
+    //Cargar aqui las imagenes
 }
 
 function setup(){
-    canvas = createCanvas(1000,800);
-
-    tom = createSprite(870, 600);
-    tom.addAnimation("tomSleeping", tomImg1);
-    tom.scale = 0.2;
-
-    jerry = createSprite(200, 600);
-    jerry.addAnimation("jerryStanding", jerryImg1);
-    jerry.scale = 0.15;
-
+    //carga los sprites de Tom y Jerry
 }
 
 function draw() {
 
-    background(bg);
-
-    if(tom.x - jerry.x < (tom.width - jerry.width)/2)
-    { 
-        tom.velocityX=0;
-        tom.addAnimation("tomLastImage", tomImg3);
-        tom.x =300;
-        tom.scale=0.2;
-        tom.changeAnimation("tomLastImage");
-        jerry.addAnimation("jerryLastImage", jerryImg3);
-        jerry.scale=0.15;
-        jerry.changeAnimation("jerryLastImage");
-    }  
-
+    background(255);
+    //escribe la condición para verificar si Tom y Jerry chocan
+    
     drawSprites();
 }
 
 function keyPressed(){
+    //escribe aqui el código para la animación del movimiento y cambio
 
-    if(keyCode === LEFT_ARROW){
-        tom.velocityX = -5; 
-        tom.addAnimation("tomRunning", tomImg2);
-        tom.changeAnimation("tomRunning");
-        
-        jerry.addAnimation("jerryTeasing", jerryImg2);
-        jerry.frameDelay = 25;
-        jerry.changeAnimation("jerryTeasing");
-    }
 }
